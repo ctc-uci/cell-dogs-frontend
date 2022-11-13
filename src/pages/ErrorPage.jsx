9a85e50ea1ec18290b45e57403f11e2bac4fc043
Kei Asakawa <77090883+Kaiyooon@users.noreply.github.com>
2022-11-12 16:09:55 -0800

    Fixed css

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
-import { Container, Image, Box, Center, VStack, Spacer } from '@chakra-ui/react';
+import { Container, Image, VStack, Center } from '@chakra-ui/react';
+import './ErrorPage.css';
 
 const ErrorPage = () => {
   return (
-    <Container h="100vh" padding="172px 68px">
-      <Container h="100%">
-        <Center h="100%" float="left" bg="red" padding="auto">
-          <Image src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
-        </Center>
-        <Spacer />
-        <Box h="100%" float="right" bg="blue" margin="auto">
-          <VStack h="100%" align="flex-start">
-            <h1>Oh no!</h1>
-            <h2>This page doesn&apos;t exist</h2>
-            <h3>Try going back to the Adoption Log</h3>
+    <Container id="window" h="100vh">
+      <Center id="container" h="100%">
+        <Image w="519px" h="332" src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
+        <Center h="100%">
+          <VStack align="flex-start" h="fit-content">
+            <h1>Add Text Here</h1>
           </VStack>
-        </Box>
-      </Container>
+        </Center>
+      </Center>
     </Container>
   );
 };
