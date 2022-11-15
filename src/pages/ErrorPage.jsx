0052fc74bfb50056fd21e0a5929d8d57f08efc8c
Kei Asakawa <77090883+Kaiyooon@users.noreply.github.com>
2022-11-15 15:35:35 -0800

    Prepared for submission

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Container, Image, VStack, Center } from '@chakra-ui/react';
 import './ErrorPage.css';
+import image2 from '../assets/images/image2.png';
 
 const ErrorPage = () => {
   return (
     <Container id="window">
       <Center id="container">
-        <Image w="519px" h="332px" src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
+        <Image w="519px" h="332px" src={image2} alt="Dan Abramov" />
         <Center h="100%">
           <VStack align="flex-start" h="fit-content">
-            <h1>Add Text Here</h1>
+            <h1>Oh no!</h1>
+            <h2>This page doesn&apos;t exist.</h2>
+            <h4>Try going back to the Adoption Log.</h4>
           </VStack>
         </Center>
       </Center>
