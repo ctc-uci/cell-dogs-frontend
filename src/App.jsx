decc96f54d1259b5151ec2d1a71a9cdc580e9cb4
Orviel Alzate <orvielalz@gmail.com>
2022-11-20 15:45:46 -0800

    changed doggo path and deleted useLocation and react fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 // import './App.css';
 import Home from './pages/Home/Home';
 import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
@@ -8,20 +8,16 @@ import AddDog from './pages/AddDog/AddDog';
 import Facilities from './pages/Facilities/Facilities';
 
 const App = () => {
-  const location = useLocation();
   return (
-    <>
-      {/* <Header /> */}
-      <Routes key={location.pathname} location={location}>
-        <Route path="/login" element={<Home />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        {/* TODO: link 404 page when merged */}
-        <Route path="/dogs" element={<Dogs />} />
-        <Route path="/dogs/new" element={<AddDog />} />
-        <Route path="/facilities" element={<Facilities />} />
-        <Route path="*" element="" />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/login" element={<Home />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/" element={<Dogs />} />
+      <Route path="/dogs/new" element={<AddDog />} />
+      <Route path="/facilities" element={<Facilities />} />
+      {/* TODO: replace element with 404 page */}
+      <Route path="*" element="" />
+    </Routes>
   );
 };
 
