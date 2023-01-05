0dbd1cbdede14feb7205ebdbfbd7cd6f0dbf3a78
evanpservito <78441979+evanpservito@users.noreply.github.com>
2023-01-05 13:26:45 -0800

    Create Navbar (#18)
    * add navbar structure, styling and react routes
    
    * add css styling for profile, navbar responsiveness to browser window adjustment, fixed cell dogs logo size
    
    * fix content overflow on browser window size adjustment
    
    * add hamburger icon and initial styling for short navbar
    
    * update short navbar css to center logo, move hamburger icon to left and profile icon and add icon to right
    
    * merge dev branch
    
    Co-authored-by: Madhu2244 <madhuxsharma@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { CookiesProvider } from 'react-cookie';
 import Home from './pages/Home/Home';
 import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
@@ -7,22 +7,22 @@ import Dogs from './pages/Dogs/Dogs';
 import AddDog from './pages/AddDog/AddDog';
 import Facilities from './pages/Facilities/Facilities';
 import ErrorPage from './pages/ErrorPage';
+import Navbar from './components/Navbar';
 import './App.css';
 import './common/global.css';
 
 const App = () => {
   return (
     <CookiesProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Home />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/" element={<Dogs />} />
-          <Route path="/dogs/new" element={<AddDog />} />
-          <Route path="/facilities" element={<Facilities />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </Router>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Home />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/" element={<Dogs />} />
+        <Route path="/dogs/new" element={<AddDog />} />
+        <Route path="/facilities" element={<Facilities />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     </CookiesProvider>
   );
 };
