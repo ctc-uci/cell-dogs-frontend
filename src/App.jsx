e32c2b418f98169b0b39b53292d11be7fa9ab7a7
Jeff Cheema <jeffcheema@Jeffs-MacBook-Air.local>
2023-01-14 01:03:56 -0800

    small cleanups + setting up new dirs + Added chakra provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { CookiesProvider } from 'react-cookie';
-import Home from './pages/Home/Home';
+import { ChakraProvider } from '@chakra-ui/react';
+// import Home from './pages/Home/Home';
 import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
 import Dogs from './pages/Dogs/Dogs';
 import AddDog from './pages/AddDog/AddDog';
@@ -10,20 +11,23 @@ import ErrorPage from './pages/ErrorPage';
 import Navbar from './components/Navbar';
 import './App.css';
 import './common/global.css';
+import LoginPage from './pages/Login/LoginPage';
 
 const App = () => {
   return (
-    <CookiesProvider>
-      <Navbar />
-      <Routes>
-        <Route path="/login" element={<Home />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/" element={<Dogs />} />
-        <Route path="/dogs/new" element={<AddDog />} />
-        <Route path="/facilities" element={<Facilities />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </CookiesProvider>
+    <ChakraProvider>
+      <CookiesProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/" element={<Dogs />} />
+          <Route path="/dogs/new" element={<AddDog />} />
+          <Route path="/facilities" element={<Facilities />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </CookiesProvider>
+    </ChakraProvider>
   );
 };
 
--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 import * as yup from 'yup';
 import { NavLink } from 'react-router-dom';
 import { Input, Stack, Button, Text } from '@chakra-ui/react';
-import cellDogsLogoHorizontal4 from '../assets/CellDogs_logo_horizontal 4.png';
-import cellDogsLogoHorizontal5 from '../assets/CellDogs_logo_horizontal 5.png';
-import loginDogImage1 from '../assets/P_Puppy_Maekawa_Genuine-removebg-preview 1.png';
-import './LoginPage.css';
+import cellDogsLogoHorizontal4 from '../../assets/CellDogs_logo_horizontal 4.png';
+import cellDogsLogoHorizontal5 from '../../assets/CellDogs_logo_horizontal 5.png';
+import loginDogImage1 from '../../assets/P_Puppy_Maekawa_Genuine-removebg-preview 1.png';
+import styles from './LoginPage.module.css';
 
 const schema = yup.object().shape({
   username: yup.string().required(),
@@ -19,36 +19,35 @@ const loginUser = async event => {
     username: event.target[0].value,
     password: event.target[1].value,
   };
-  const isValid = await schema.isValid(formData);
-  console.log(isValid);
+  await schema.isValid(formData);
 };
 
 const LoginPage = () => {
   return (
     <div className="login-page">
       <div className="information">
-        <img className="login-dog-image-1" src={loginDogImage1} alt="loginDogImage1" />
+        <img className={styles['login-dog-image-1']} src={loginDogImage1} alt="loginDogImage1" />
         <Text>Welcome to the Adoption Log!</Text>
       </div>
-      <div className="login">
+      <div className={styles.login}>
         <Stack spacing={3} align="center">
           <NavLink to="/">
             <img
-              className="cds-logo-horizontal-4"
+              className={styles['cds-logo-horizontal-4']}
               src={cellDogsLogoHorizontal4}
               alt="cellDogsLogoHorizontal4"
             />
             <img
-              className="cds-logo-horizontal-5"
+              className={styles['cds-logo-horizontal-5']}
               src={cellDogsLogoHorizontal5}
               alt="cellDogsLogoHorizontal4"
             />
           </NavLink>
-          <form className="input-form" onSubmit={loginUser}>
+          <form className={styles['input-form']} onSubmit={loginUser}>
             <Input htmlSize={50} width="auto" placeholder="Username" size="md" />
             <Input htmlSize={50} width="auto" placeholder="Password" size="md" type="password" />
             <Button
-              className="submit-button"
+              className={styles['submit-button']}
               bg="CDSBlue1"
               color="white"
               variant="solid"
