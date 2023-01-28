6b278745c1afed6e0ffc7ce4fce4935acdee517a
RaymondYan38 <raymondyan17@gmail.com>
2023-01-27 22:09:52 -0800

    begin auth provider using firebase, implement into login page

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 // import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
-import { NavLink } from 'react-router-dom';
-import { Input, Stack, Button, Text } from '@chakra-ui/react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { Input, Stack, Button, Text, Alert, AlertIcon, AlertTitle } from '@chakra-ui/react';
 import cellDogsLogoHorizontal4 from '../../assets/CellDogs_logo_horizontal 4.png';
 import cellDogsLogoHorizontal5 from '../../assets/CellDogs_logo_horizontal 5.png';
 import loginDogImage1 from '../../assets/P_Puppy_Maekawa_Genuine-removebg-preview 1.png';
 import styles from './LoginPage.module.css';
+import { useAuth } from '../../contexts/AuthContext';
 
 const schema = yup.object().shape({
   username: yup.string().required(),
   password: yup.string().min(4).max(10).required(),
 });
 
-const loginUser = async event => {
-  event.preventDefault();
-  const formData = {
-    username: event.target[0].value,
-    password: event.target[1].value,
-  };
-  await schema.isValid(formData);
-};
-
 const LoginPage = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { login } = useAuth();
+
+  const loginUser = async event => {
+    event.preventDefault();
+    const formData = {
+      username: event.target[0].value,
+      password: event.target[1].value,
+    };
+    await schema.isValid(formData);
+
+    try {
+      setError('');
+      setLoading(true);
+      await login(emailRef.current.value, passwordRef.current.value);
+      navigate('/');
+    } catch (e) {
+      setError('Failed to log in');
+    }
+    setLoading(false);
+  };
+
   return (
     <div className="login-page">
       <div className="information">
@@ -44,9 +62,17 @@ const LoginPage = () => {
             />
           </NavLink>
           <form className={styles['input-form']} onSubmit={loginUser}>
-            <Input htmlSize={50} width="auto" placeholder="Username" size="md" />
-            <Input htmlSize={50} width="auto" placeholder="Password" size="md" type="password" />
+            <Input htmlSize={50} width="auto" placeholder="Username" size="md" ref={emailRef} />
+            <Input
+              htmlSize={50}
+              width="auto"
+              placeholder="Password"
+              size="md"
+              type="password"
+              ref={passwordRef}
+            />
             <Button
+              disabled={loading}
               className={styles['submit-button']}
               bg="CDSBlue1"
               color="white"
@@ -56,6 +82,12 @@ const LoginPage = () => {
               Log in
             </Button>
           </form>
+          {error && (
+            <Alert status="error" width={200} justifyContent="center">
+              <AlertIcon />
+              <AlertTitle>{error}</AlertTitle>
+            </Alert>
+          )}
           <NavLink to="/forgot-password">Forgot Password?</NavLink>
         </Stack>
       </div>
