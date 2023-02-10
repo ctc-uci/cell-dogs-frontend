6f1ba3ce2e02bf203bef4ba702b83d8a7ad80820
Jeff Cheema <jeffcheema007@gmail.com>
2023-02-10 15:39:18 -0800

    local changes saved

--- src/pages/Facilities/Facilities.jsx
+++ src/pages/Facilities/Facilities.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import { Button } from '@chakra-ui/react';
-import { useNavigate } from 'react-router-dom';
 import Location from '../../components/Location';
 
 const Facilities = () => {
-  const Navigation = useNavigate();
   return (
     <div>
       <p>This is the Facilities page</p>
       <Location />
-      <Button>
-        <div>
-          <Button onClick={() => Navigation('/add-facility')}>Add Facility</Button>
-        </div>
-      </Button>
     </div>
   );
 };
