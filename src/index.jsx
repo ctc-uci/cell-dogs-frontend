8797ebf6bb471b0373bb2403b9f2e07d0a3af41a
Alan Chang <alan@alanchang.xyz>
2023-02-15 16:00:34 -0800

    Fix dependencies and bump react to 18 to fix compilation issues

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <Router>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
+      <App />
     </Router>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
