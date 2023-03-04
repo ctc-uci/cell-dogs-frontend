63e535242aa6e1feaf055e52184018869503f222
“Michelle <itsmichellelin@gmail.com>
2023-03-04 11:58:16 -0800

    finish styling and start backend connection

--- src/pages/AddDog/AddDog.jsx
+++ src/pages/AddDog/AddDog.jsx
@@ -1,22 +1,97 @@
-import React from 'react';
+import { React } from 'react';
 import {
   FormControl,
   FormLabel,
   Input,
   Button,
+  ButtonGroup,
   Heading,
   Select,
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
+  Avatar,
 } from '@chakra-ui/react';
 import { ArrowBackIcon, ChevronRightIcon, AddIcon } from '@chakra-ui/icons';
-import cellDogsSampleProfilePicture from '../../assets/CellDogs_sample_profile_picture.png';
 import './AddDog.css';
-
+import { useBackend } from '../../contexts/BackendContext';
 // import Location from '../../components/Location';
 
 const AddDog = () => {
+  // const [facilityid, setFacilityID] = useState(0);
+  // const [groupnum, setGroupNum] = useState(0);
+  // const [graddate, setGradDate] = useState('');
+  // const [dogname, setDogName] = useState('');
+  // const [age, setAge] = useState(0);
+  // const [shelter, setShelter] = useState('');
+  // const [breed, setBreed] = useState('');
+  // const [chiptype, setChipType] = useState('');
+  // const [chipnum, setChipNum] = useState(0);
+  // const [gender, setGender] = useState('');
+  // const [profilepic, setProfilePic] = useState('');
+  // const [altname, setAltName] = useState('');
+  // const [notes, setNotes] = useState('');
+  // const [adoptername, setAdopterName] = useState('');
+  // const [adopterphone, setAdopterPhone] = useState('');
+  // const [addrline, setAddrLine] = useState('');
+  // const [adoptcity, setAdoptCity] = useState('');
+  // const [adoptstate, setAdoptState] = useState('');
+  // const [zip, setZip] = useState('');
+  // const [adoptemail, setAdoptEmail] = useState('');
+  // const [fees, setFees] = useState(0);
+  // const [revenue, setRevenue] = useState(0);
+  const { backend } = useBackend();
+
+  backend
+    .post('http://localhost:3001/dog')
+    .then(response => {
+      const jsonData = response.data;
+      console.log(jsonData); // or do something else with the data
+    })
+    .catch(error => {
+      console.error(error);
+    });
+
+  // const saveAllChanges = async () => {
+  //   const dogs = await backend.get('/dog');
+  //   const dogNames = dogs.data.map(dog => dog.dogname);
+
+  //   if (dogNames.indexOf(dogname) > -1) {
+  //     // dog already in table
+  //     // update row
+  //     return;
+  //   }
+
+  //   backend
+  //     .post('/dog', {
+  //       // facilityid,
+  //       // groupnum,
+  //       graddate,
+  //       dogname,
+  //       // age,
+  //       // shelter,
+  //       breed,
+  //       // chiptype,
+  //       // chipnum,
+  //       gender,
+  //       // profilepic,
+  //       altname,
+  //       // notes,
+  //       adoptername,
+  //       adopterphone,
+  //       // addrline,
+  //       // adoptcity,
+  //       // adoptstate,
+  //       // zip,
+  //       adoptemail,
+  //       // fees,
+  //       // revenue,
+  //     })
+  //     .then(() => {
+  //       console.log('Successfully updated dog');
+  //     });
+  // };
+
   return (
     <div>
       {/* <Location /> */}
@@ -28,7 +103,7 @@ const AddDog = () => {
             </BreadcrumbItem>
 
             <BreadcrumbItem>
-              <BreadcrumbLink href="#">Lucky</BreadcrumbLink>
+              <BreadcrumbLink href="#">New Dog</BreadcrumbLink>
             </BreadcrumbItem>
           </Breadcrumb>
         </div>
@@ -45,16 +120,52 @@ const AddDog = () => {
               Go Back
             </Button>
           </div>
-          <img id="profilePic" src={cellDogsSampleProfilePicture} alt="Cell Dogs Sample Profile" />
+          <Avatar height="100px" width="100px" />
         </div>
-        <h2 className="dogName">Lucky</h2>
+        <div className="name">
+          <div className="nameInput">
+            <FormControl>
+              <Input
+                id="nameField"
+                type="name"
+                placeholder="Enter Name"
+                size="lg"
+                variant="unstyled"
+                // onChange={e => {
+                //   setDogName(e.target.value);
+                // }}
+              />
+            </FormControl>
+          </div>
+        </div>
+        <div className="addTag">
+          <Select placeholder="Add Tag" isMulti>
+            <option value="Service">Service</option>
+            <option value="Therapy">Therapy</option>
+            <option value="Staff Adoption">Staff Adoption</option>
+            <option value="Special Needs">Special Needs</option>
+            <option value="Deceased">Deceased</option>
+          </Select>
+        </div>
         <div className="tagRow">
           <div className="tag">
             <h3>Service</h3>
           </div>
         </div>
-        <div className="editDogButton">
-          <Button>Edit Dog</Button>
+        <div className="buttons">
+          <div className="cancelButton">
+            <ButtonGroup variant="outline" spacing="6">
+              <Button>Cancel</Button>
+            </ButtonGroup>
+          </div>
+          <div className="removeDogButton">
+            <ButtonGroup variant="outline" spacing="6">
+              <Button colorScheme="red">Remove Dog</Button>
+            </ButtonGroup>
+          </div>
+          <div className="saveButton">
+            <Button colorScheme="facebook">Save All Changes</Button>
+          </div>
         </div>
       </div>
       <div className="row1">
@@ -64,15 +175,33 @@ const AddDog = () => {
           </Heading>
           <FormControl>
             <FormLabel>Name</FormLabel>
-            <Input type="text" />
+            <Input
+              type="text"
+              className="formInput"
+              // onChange={e => {
+              //   setAdopterName(e.target.value);
+              // }}
+            />
           </FormControl>
           <FormControl>
             <FormLabel>Email</FormLabel>
-            <Input type="email" />
+            <Input
+              type="email"
+              className="formInput"
+              // onChange={e => {
+              //   setAdoptEmail(e.target.value);
+              // }}
+            />
           </FormControl>
           <FormControl>
             <FormLabel>Phone</FormLabel>
-            <Input type="text" />
+            <Input
+              type="text"
+              className="formInput"
+              // onChange={e => {
+              //   setAdopterPhone(e.target.value);
+              // }}
+            />
           </FormControl>
         </div>
 
@@ -82,33 +211,57 @@ const AddDog = () => {
           </Heading>
           <FormControl>
             <FormLabel>Alternate Name</FormLabel>
-            <Input type="text" />
+            <Input
+              type="text"
+              className="formInput"
+              // onChange={e => {
+              //   setAltName(e.target.value);
+              // }}
+            />
           </FormControl>
           <FormControl>
             <FormLabel>Breed</FormLabel>
-            <Input type="text" />
+            <Input
+              type="text"
+              className="formInput"
+              // onChange={e => {
+              //   setBreed(e.target.value);
+              // }}
+            />
           </FormControl>
           <div className="genderAndGrad">
             <div className="gender">
               <FormLabel>Gender</FormLabel>
-              <Select placeholder="Select Gender">
+              <Select
+                placeholder="Select Gender"
+                className="formInput"
+                // onChange={e => {
+                //   setGender(e.target.value);
+                // }}
+              >
                 <option value="Male">Male</option>
                 <option value="Female">Female</option>
               </Select>
             </div>
             <FormControl className="grad">
               <FormLabel>Graduation Age</FormLabel>
-              <Input type="text" />
+              <Input
+                type="text"
+                className="formInput"
+                // onChange={e => {
+                //   setGradDate(e.target.value);
+                // }}
+              />
             </FormControl>
           </div>
           <div className="chipInputFields">
             <FormControl className="chipType">
               <FormLabel>Chip Type</FormLabel>
-              <Input type="text" />
+              <Input type="text" className="formInput" />
             </FormControl>
             <FormControl>
               <FormLabel>Chip Number</FormLabel>
-              <Input type="text" />
+              <Input type="text" className="formInput" />
             </FormControl>
           </div>
         </div>
@@ -120,21 +273,21 @@ const AddDog = () => {
           </Heading>
           <FormControl>
             <FormLabel>Address</FormLabel>
-            <Input type="text" />
+            <Input type="text" className="formInput" />
           </FormControl>
           <div className="cityAndState">
             <FormControl className="city">
               <FormLabel>City</FormLabel>
-              <Input type="text" />
+              <Input type="text" className="formInput" />
             </FormControl>
             <FormControl>
               <FormLabel>State</FormLabel>
-              <Input type="text" />
+              <Input type="text" className="formInput" />
             </FormControl>
           </div>
           <FormControl>
             <FormLabel>Zip Code</FormLabel>
-            <Input type="text" />
+            <Input type="text" className="formInput" />
           </FormControl>
           <div className="financial">
             <Heading as="h2" fontSize="24px">
@@ -143,11 +296,11 @@ const AddDog = () => {
             <div className="financialFields">
               <FormControl className="fees">
                 <FormLabel>Fees</FormLabel>
-                <Input type="text" />
+                <Input type="text" className="formInput" />
               </FormControl>
               <FormControl>
                 <FormLabel>Revenue</FormLabel>
-                <Input type="text" />
+                <Input type="text" className="formInput" />
               </FormControl>
             </div>
           </div>
@@ -158,31 +311,31 @@ const AddDog = () => {
             Facility Info
           </Heading>
           <FormLabel>Facility</FormLabel>
-          <Select placeholder="Select Facility">
+          <Select placeholder="Select Facility" className="formInput">
             <option value="Male">Male</option>
             <option value="Female">Female</option>
           </Select>
           <FormControl>
             <FormLabel>Facility Unit</FormLabel>
-            <Input type="text" />
+            <Input type="text" className="formInput" />
           </FormControl>
           <div className="GradAndGroup">
             <FormControl className="gradDate">
               <FormLabel>Graduation Date</FormLabel>
-              <Input type="text" />
+              <Input type="text" className="formInput" />
             </FormControl>
             <FormControl>
               <FormLabel>Group Number</FormLabel>
-              <Input type="text" />
+              <Input type="text" className="formInput" />
             </FormControl>
           </div>
           <FormControl>
             <FormLabel>Shelter</FormLabel>
-            <Input type="text" />
+            <Input type="text" className="formInput" />
           </FormControl>
           <FormControl>
             <FormLabel>Animal ID</FormLabel>
-            <Input type="text" />
+            <Input type="text" className="formInput" />
           </FormControl>
         </div>
       </div>
