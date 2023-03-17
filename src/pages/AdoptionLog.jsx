c00444bde28051d24347bb9a0abf1124a534ad6f
Myron Zhou <myronzhou@Myrons-MacBook-Air.local>
2023-03-16 21:49:16 -0700

    added adoptionlogtables

--- src/pages/AdoptionLog.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-// import { DropDownList } from "@progress/kendo-react-dropdowns";
-
-const AdoptionLog = () => {
-  return <div>Adoption Log</div>;
-};
-
-export default AdoptionLog;
