5bb288cf07c881bf605f064cf31f40c7da4a03a9
kadejna <78989039+kadejna@users.noreply.github.com>
2023-03-18 15:30:57 -0700

    subnavbar design

--- src/pages/Dogs/Dogs.jsx
+++ src/pages/Dogs/Dogs.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Button } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
+import { AddIcon } from '@chakra-ui/icons';
 import Location from '../../components/Location';
 import { useAuth } from '../../contexts/AuthContext';
 import BreadcrumbBar from '../../components/BreadcrumbBar/BreadcrumbBar';
-import { AddIcon } from '@chakra-ui/icons';
+// eslint-disable-next-line import/no-useless-path-segments
+import AdoptionLogNavbar from '../../pages/AdoptionLog';
 
 const Dogs = () => {
   const { currentUser, logout } = useAuth();
@@ -15,15 +17,14 @@ const Dogs = () => {
     navigate('/login');
   };
 
-
   return (
     <div>
       <div className="breadcrumbAndAdd">
         <BreadcrumbBar left="Adoption Log">
           <div className="addDogButton">
-            <Button 
-              leftIcon={<AddIcon />} 
-              size="sm" 
+            <Button
+              leftIcon={<AddIcon />}
+              size="sm"
               onClick={() => {
                 navigate('/dogs/new');
               }}
@@ -33,6 +34,8 @@ const Dogs = () => {
           </div>
         </BreadcrumbBar>
       </div>
+      <AdoptionLogNavbar />
+      <hr />
       <p>This is the Dog Table page</p>
       <strong>User email/username:</strong>
       {currentUser.email}
