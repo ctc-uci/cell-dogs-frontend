06800cd51b0d509287209f5f8e5461a77b085b4b
liannejl <liannejl@uci.edu>
2023-05-01 23:54:35 -0700

    finished trying to implement useFrom for LoginPage but still doesn't work

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,20 +1,24 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, {
+  // useState,
+  useEffect,
+  // useRef
+} from 'react';
 // import PropTypes from 'prop-types';
 // import { useForm } from 'react-hook-form';
-// import * as yup from 'yup';
+import * as yup from 'yup';
 import { useForm } from 'react-hook-form';
-// import { yupResolver } from '@hookform/resolvers/yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { NavLink, useNavigate } from 'react-router-dom';
 import {
   Input,
   Stack,
   Button,
   Text,
-  Alert,
-  AlertIcon,
-  AlertTitle,
+  // Alert,
+  // AlertIcon,
+  // AlertTitle,
   FormControl,
-  FormLabel,
+  // FormLabel,
   FormErrorMessage,
 } from '@chakra-ui/react';
 import cellDogsLogoHorizontal4 from '../../assets/CellDogs_logo_horizontal-4.png';
@@ -23,10 +27,10 @@ import loginDogImage1 from '../../assets/P_Puppy_Maekawa_Genuine-removebg-previe
 import styles from './LoginPage.module.css';
 import { useAuth } from '../../contexts/AuthContext';
 
-// const schema = yup.object().shape({
-//   username: yup.string().required(),
-//   password: yup.string().min(4).max(18).required(),
-// });
+const schema = yup.object().shape({
+  username: yup.string().required(),
+  password: yup.string().min(4).max(18).required(),
+});
 // const Login = ({ setModalStep, onClose, info, setRender, render }) => {
 //   const [email, password] = useState({
 //     fullName: `${info.firstName} ${info.lastName}`,
@@ -40,8 +44,8 @@ import { useAuth } from '../../contexts/AuthContext';
 // };
 
 const LoginPage = () => {
-  const emailRef = useRef();
-  const passwordRef = useRef();
+  // const emailRef = useRef();
+  // const passwordRef = useRef();
   // const {
   //   register,
   //   handleSubmit,
@@ -52,14 +56,15 @@ const LoginPage = () => {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm({
-    // resolver: yupResolver(schema),
+    resolver: yupResolver(schema),
   });
   const navigate = useNavigate();
-  const [error, setError] = useState('');
+  // const [error, setError] = useState('');
   // const [loading, setLoading] = useState(true);
   // const { currentUser, login } = useAuth();
-  const { currentUser } = useAuth();
+  const { currentUser, login } = useAuth();
   /* const loginUser = async event => {
     event.preventDefault();
     const formData = {
@@ -77,24 +82,31 @@ const LoginPage = () => {
     }
   }; */
   // const onSubmit = data => console.log(data);
-  const onSubmit = async event => {
-    event.preventDefault();
-    // const { email, password } = event;
+  const onSubmitHandler = async data => {
+    // event.preventDefault();
+    console.log('hello');
+    const { email, password } = data;
     // const formData = {
     //   username: event.target[0].value,
     //   password: event.target[1].value,
     // };
-    console.log('hello');
+    // const formData = {
+    //   username: email,
+    //   password: givenPassword,
+    // };
+    await login(email, password);
+    navigate('/');
+    reset();
     // await schema.isValid(formData);
-    console.log(event.target[0].value);
-    console.log(event.target[1].value);
-    try {
-      setError('');
-      await register(emailRef, passwordRef);
-      navigate('/');
-    } catch (e) {
-      setError('Failed to log in');
-    }
+    // console.log(event.target[0].value);
+    // console.log(event.target[1].value);
+    // try {
+    //   setError('');
+    //   await login(email, givenPassword);
+    //   navigate('/');
+    // } catch (e) {
+    //   setError('Failed to log in');
+    // }
   };
 
   // const inputChange = () => {
@@ -135,12 +147,12 @@ const LoginPage = () => {
             </NavLink>
           </div>
           {/* Form Control for every input */}
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <FormControl isInvalid={errors?.fullName} className={styles['input-form']}>
-              <FormLabel>Email</FormLabel>
+          <form className={styles['input-form']} onSubmit={handleSubmit(onSubmitHandler)}>
+            <FormControl isInvalid={errors?.email} className={styles['input-form']}>
+              {/* <FormLabel>Email</FormLabel> */}
               {/* Notice how we dont need to use states */}
               {/* <Input {...register('email')} /> */}
-              <Input
+              {/* <Input
                 htmlSize={50}
                 width="auto"
                 placeholder="Email"
@@ -154,7 +166,15 @@ const LoginPage = () => {
                     ref: { emailRef },
                   }).ref
                 }
+              /> */}
+              <Input
+                htmlSize={50}
+                width="auto"
+                placeholder="Email"
+                size="md"
+                {...register('email')}
               />
+              <FormErrorMessage>{errors?.email && errors?.email?.message}</FormErrorMessage>
             </FormControl>
             <FormControl isInvalid={errors?.password} className={styles['input-form']}>
               <Input
@@ -163,16 +183,18 @@ const LoginPage = () => {
                 placeholder="Password"
                 size="md"
                 type="password"
+                {...register('password')}
                 // ref={passwordRef}
                 // onChange={inputChange}
-                ref={
-                  register('password', {
-                    required: true,
-                    pattern: /^.{6,}$/,
-                    ref: { passwordRef },
-                  }).ref
-                }
+                // ref={
+                //   register('password', {
+                //     required: true,
+                //     pattern: /^.{6,}$/,
+                //     ref: { passwordRef },
+                //   }).ref
+                // }
               />
+              <FormErrorMessage>{errors?.password && errors?.password?.message}</FormErrorMessage>
             </FormControl>
             <Button
               // isDisabled={loading}
@@ -190,7 +212,6 @@ const LoginPage = () => {
             >
               Log in
             </Button>
-            <FormErrorMessage>{errors?.email && errors?.fullName?.message}</FormErrorMessage>
           </form>
           {/* <form className={styles['input-form']} onSubmit={loginUser}>
             <Input
@@ -221,55 +242,17 @@ const LoginPage = () => {
               Log in
             </Button> */}
           {/* </form> */}
-          {error && (
+          {/* {error && (
             <Alert status="error" width={200} justifyContent="center">
               <AlertIcon />
               <AlertTitle>{error}</AlertTitle>
             </Alert>
-          )}
+          )} */}
           <NavLink to="/forgot-password">Forgot Password?</NavLink>
         </Stack>
       </div>
     </div>
   );
 };
-
-// LoginPage.propTypes = {
-//   formData: PropTypes.shape({
-//     username: PropTypes.string,
-//     password: PropTypes.string,
-//   }).isRequired,
-// }
-
-// LoginPage.propTypes = {
-//   formData: PropTypes.shape({
-//     username: PropTypes.string,
-//     password: PropTypes.string,
-//   }).isRequired,
-// currentUser: PropTypes.func.isRequired,
-// setRender: PropTypes.func.isRequired,
-// render: PropTypes.bool.isRequired,
-// };
-
-// LoginPage.propTypes = {
-//   formData: PropTypes.shape(formDataShape).isRequired,
-//   currentUser: PropTypes.func.isRequired,
-// };
-
-// EditUser.propTypes = {
-//   setModalStep: PropTypes.func.isRequired,
-//   onClose: PropTypes.func.isRequired,
-//   info: PropTypes.shape({
-//     firstName: PropTypes.string,
-//     lastName: PropTypes.string,
-//     email: PropTypes.string,
-//   }).isRequired,
-// };
-
-// LoginPage.propTypes = {
-//   setModalStep: PropTypes.func.isRequired,
-//   onClose: PropTypes.func.isRequired,
-//   onSubmit: PropTypes.func.isRequired,
-// };
 
 export default LoginPage;
